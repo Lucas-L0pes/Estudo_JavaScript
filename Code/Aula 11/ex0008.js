62df06cb7b4f4502849c952d6c3fd3b3e6a86f60
Lucas-L0pes <lucas.no16@hotmail.com>
2023-06-27 20:51:26 -0300

    Removendo arquivos de aulas

--- Code/Aula 11/ex0008.js
+++
@@ -1,7 +0,0 @@
-var vel = 12 
-console.log(`a velocidade do seu carro é ${vel} km/h`)
-
-if (vel> 60){
-    console.log(`Você ultrapassou o limite de velocidade`)
-}
-    console.log(`Dirija-se sempre usando o sinto de segurança!`)
--- Code/Aula 16/função01.js
+++
@@ -1,8 +0,0 @@
-function parimpar(n){
-    if(n % 2 == 0 ){
-        return 'par'
-    }else{
-        return 'impar'
-    }   
-}
- console.log(parimpar(223))
